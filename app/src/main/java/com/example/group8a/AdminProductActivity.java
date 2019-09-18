d8e3e992f00f8da290bd8dafa72df06e1f09256f
RehaniH <rehani44perera@gmail.com>
2019-09-18 16:20:44 +0530

    Second Commit

--- app/src/main/java/com/example/group8a/AdminProductActivity.java
+++ app/src/main/java/com/example/group8a/AdminProductActivity.java
@@ -1,22 +1,49 @@
 package com.example.group8a;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class AdminProductActivity extends AppCompatActivity {
+
+    private static final int  PICK_IMAGE_REQUEST = 1;
+
+    Button choose;
+    Button upload;
+    TextView show;
+    EditText imageName;
+    ImageView imageView;
+    ProgressBar progressBar;
+    private Uri imageUri;
+
+    private StorageReference sRef;
+    private DatabaseReference dataRef;
 
     EditText textName, textColor, textCate, textQty;
     Button btnSave, btnShow, btnUpdate, btnDelete;
@@ -33,9 +60,95 @@ public class AdminProductActivity extends AppCompatActivity {
         textQty = findViewById(R.id.quantity);
 
         btnSave = findViewById(R.id.btnSave);
-
         product = new Product();
 
+
+        choose = findViewById(R.id.btn_choose);
+        upload = findViewById(R.id.btn_upload);
+        show = findViewById(R.id.Show_uploads);
+        imageName = findViewById(R.id.image_name);
+        imageView = findViewById(R.id.image_to_upload);
+        progressBar = findViewById(R.id.progress_bar);
+
+        sRef = FirebaseStorage.getInstance().getReference("uploads");
+        dataRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    openFileChooser();
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void openFileChooser(){
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+        && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            imageView.setImageURI(imageUri);
+
+        }
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cr  = getContentResolver();
+        MimeTypeMap mimeMap = MimeTypeMap.getSingleton();
+        return mimeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void uploadFile(){
+
+        if(imageUri != null){
+            StorageReference fileReference = sRef.child(System.currentTimeMillis() + "." +
+                    getFileExtension(imageUri));
+            fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AdminProductActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                    progressBar.setProgress((int)progress);
+
+                }
+            });
+        }else
+            Toast.makeText(this, "No Image File Selected", Toast.LENGTH_SHORT).show();
     }
 
     public void onClickSave(View view){
